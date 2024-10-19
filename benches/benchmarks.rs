107618529f14ec2c7b95acdeff2ee88ddad8efec
Davide Del Papa <davidedelpapa@yahoo.com>
2024-10-19 11:07:20 +0200

    updated benches

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -6,8 +6,10 @@ fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| {
             //greet(black_box("Alice"))
             let config = ParserConfig::new();
-            let _parser = Parser::new().set_config(config);
-            //parser.parse();
+            let mut parser = Parser::new()
+                .set_config(config)
+                .statement("CREATE TABLE person ( Id INTEGER NOT NULL, name VARCHAR(255) )");
+            _ = parser.parse();
         })
     });
 }
